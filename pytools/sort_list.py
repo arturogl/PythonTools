db4947e00a1748bf90d6211378ff574051415211
Arturo Gutierrez Loza <arturogl@mac.com>
2024-04-15 07:03:23 -0600

    Create sort_list.py

---
+++ pytools/sort_list.py
@@ -0,0 +1,12 @@
+# Sample list
+my_list = [3, 1, 2]
+
+# Sort the list
+sorted_list = sorted(my_list)
+
+print("Sorted list:", sorted_list)
+
+# Using sorted() function with reverse parameter
+sorted_list_desc = sorted(my_list, reverse=True)
+
+print("Sorted list desc:", sorted_list_desc)
